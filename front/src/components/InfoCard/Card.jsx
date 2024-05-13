8e718e0d8795abac680321ccaec591f9b1257372
Bariy <bladflag8@gmail.com>
2024-05-13 18:06:10 +0300

    a

--- front/src/components/InfoCard/Card.jsx
+++ front/src/components/InfoCard/Card.jsx
@@ -5,9 +5,11 @@ import { homeData } from "../../data/homeData";
 export default function Card() {
     const { id } = useParams()
     const cardInfo = homeData.cards.find(card => card.id === parseInt(id))
+
     if (!cardInfo) {
         return <div>Card not found</div>
     }
+
     return (
         <div className="card" key={cardInfo.id}>
             <img
@@ -18,24 +20,30 @@ export default function Card() {
                 <div className="card__info-desc1">
                     <p>Rating: {cardInfo.rating}</p>
                     <p>Genre: {cardInfo.genre}</p>
+                    <p>Tags:
+                        <span>{cardInfo.tags[0].tag1},</span>
+                        <span>{cardInfo.tags[0].tag2},</span>
+                        <span>{cardInfo.tags[0].tag3}</span>
+                    </p>
+                    <p>Starring: {cardInfo.starring}</p>
                     <p>Time: {cardInfo.time}</p>
                     <p>Year: {cardInfo.year}</p>
                 </div>
                 <p className="card__info-desc2">
                     {cardInfo.desc2}
                 </p>
-                <iframe
-                    width="760"
-                    height="490"
-                    src={cardInfo.video}
-                    title={cardInfo.name}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; 
-                encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                ></iframe>
             </div>
+            <iframe
+                width="1100"
+                height="610"
+                src={cardInfo.video}
+                title={cardInfo.name}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; 
+                encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            ></iframe>
         </div>
 
     );
